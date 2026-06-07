8bf47df75040cf875ecaca10adb56bdd50eb9935
Titima <colonruizanabel@gmail.com>
2026-06-06 23:46:12 -0400

    Add extensions.js with various extension names

---
+++ packages/scratch-gui/src/extensions.js
@@ -0,0 +1,33 @@
+{
+  Music
+},
+{
+  Pen
+},
+  {
+  Video Sensing
+},
+  {
+  Text to Speech
+},
+  {
+  Translate
+},
+  {
+  Makey Makey
+},
+  {
+  micro:bit
+},
+  {
+  LEGO MINDSTORMS EV3
+},
+  {
+  LEGO BOOST
+},
+{
+  LEGO Education WeDo 2.0
+},
+  {
+Go Direct Force & Acceleration
+}
